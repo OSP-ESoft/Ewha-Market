386d6da564326386e12bac33b2d77d07b62fc0ad
ddioni <beth020207@gmail.com>
2024-11-19 15:56:30 +0900

    week 11 login done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session
 from database import DBhandler
 import sys
 import hashlib
@@ -18,14 +18,24 @@ def loginpage():
 def mypage():
     return render_template("mypage.html")
 
-@application.route("/login")
+@application.route("/login", methods=['POST'])
 def login():
     flash("로그인")
-    return render_template("index.html")
+    id_=request.form['id']
+    pw=request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.find_user(id_,pw_hash):
+        session['id']=id_
+        return render_template("index.html")
+    else:
+        flash("잘못된 값입니다.")
+        return render_template("login.html")
+    
 
 @application.route("/logout")
 def logout():
     flash("로그아웃")
+    session.clear()
     return render_template("index.html")
 
 @application.route("/registerpage")
@@ -47,7 +57,50 @@ def register():
 
 @application.route("/list")
 def view_list():
-    return render_template("list.html")
+    page = request.args.get("page", 1, type=int)
+
+    per_page = 10
+    per_row = 5
+    row_count = int(per_page/per_row)
+
+    #보여줄 페이지의 첫/마지막 상품 인덱스
+    start_idx = per_page*(page-1)
+    end_idx = per_page*(page)
+
+    data = DB.get_items() #read table
+
+    item_counts = len(data)
+    data = dict(list(data.items())[start_idx:end_idx])
+
+    total_count = len(data)
+    rows = []  # 상품을 row 단위로 나누어 저장할 리스트
+
+    for i in range(row_count):
+        if (i == row_count - 1) and (total_count % per_row != 0):  # 마지막 줄
+            rows.append(dict(list(data.items())[i * per_row:])) 
+        else:
+            rows.append(dict(list(data.items())[i * per_row:(i + 1) * per_row]))
+    
+    return render_template(
+        "list.html",
+        # datas = data.items(),
+        rows = rows,
+        limit = per_page,
+        page = page,
+        page_count = int((item_counts/per_page)+1),
+        total = item_counts)
+
+#상세페이지 라우팅
+@application.route('/dynamicurl/<varible_name>/')
+def DynamicUrl(varible_name):
+    return str(varible_name)
+
+@application.route("/view_detail/<name>/")
+def view_item_detail(name):
+    print("###name:",name)
+    data = DB.get_item_byname(str(name))
+    print("####data:",data)
+    return render_template("detail.html", name=name, data=data)
 
 @application.route("/review")
 def view_review():
@@ -64,8 +117,6 @@ def reg_item():
 @application.route("/reg_reviews")
 def reg_review():
     return render_template("reg_reviews.html")
-
-
 
 
 @application.route("/submit_item", methods=['POST'])
