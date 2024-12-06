e76ca32aec8e34bd7309080c3eff876e05451b22
jieun-ewha <2476124@ewha.ac.kr>
2024-12-06 13:59:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -37,11 +37,6 @@ def logout():
     session.clear()
     return render_template("index.html")
    
-
-@application.route("/mypage")
-def mypage():
-    return render_template("mypage.html")
-
 #회원가입
 @application.route("/registerpage")
 def registerpage():
